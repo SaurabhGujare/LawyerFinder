a920a2b34c1eed1a18e7de228b091652c04fa7b0
ninadsubhedar <subhedar.n@husky.neu.edu>
2018-11-26 22:50:40 -0500

    Project - Legal Entity, Address, ContactDetails POJO

--- JusticeFinder/src/app/entities/LegalEntity.java
+++ JusticeFinder/src/app/entities/LegalEntity.java
@@ -5,10 +5,65 @@
  */
 package app.entities;
 
+import java.util.Date;
+
 /**
  *
  * @author Ninad Subhedar (NUID : 001472377)
  */
 public class LegalEntity {
     
+    private String firstName, middleName, lastName;
+    private Date dob;
+    Address priamryAddress;
+    ContactDetails primaryContact;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getDob() {
+        return dob;
+    }
+
+    public void setDob(Date dob) {
+        this.dob = dob;
+    }
+
+    public Address getPriamryAddress() {
+        return priamryAddress;
+    }
+
+    public void setPriamryAddress(Address priamryAddress) {
+        this.priamryAddress = priamryAddress;
+    }
+
+    public ContactDetails getPrimaryContact() {
+        return primaryContact;
+    }
+
+    public void setPrimaryContact(ContactDetails primaryContact) {
+        this.primaryContact = primaryContact;
+    }
+            
 }
