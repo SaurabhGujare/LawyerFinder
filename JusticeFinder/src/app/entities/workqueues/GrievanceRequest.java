653e1569c5d2efe1a7e8c48e59e6b7554449c2b8
Saurabh Nitin Gujare <gujare.s@husky.neu.edu>
2018-12-02 12:48:49 -0500

    JusticeFinder[Code]:Extended GreivanceRequest From WorkItem. And made necessary changes.

--- JusticeFinder/src/app/entities/workqueues/GrievanceRequest.java
+++ JusticeFinder/src/app/entities/workqueues/GrievanceRequest.java
@@ -6,17 +6,20 @@
 package app.entities.workqueues;
 
 import app.entities.user.LegalEntity;
+import app.entities.user.UserAccount;
 import java.util.Date;
 
 /**
  *
  * @author Saurabh Gujare (NUID : 001424874)
  */
-public class GrievanceRequest {
+public class GrievanceRequest extends WorkItem {
     private LegalEntity legalEntity;
-    private String requestMessage;
-    private int requestId;
-    private Date requestDate;
+
+
+    public GrievanceRequest(String message, UserAccount sender, UserAccount receiver) {
+        super(message, sender, receiver);
+    }
 
     public LegalEntity getLegalEntity() {
         return legalEntity;
@@ -24,31 +27,5 @@ public class GrievanceRequest {
 
     public void setLegalEntity(LegalEntity legalEntity) {
         this.legalEntity = legalEntity;
-    }
-
-    public String getRequestMessage() {
-        return requestMessage;
-    }
-
-    public void setRequestMessage(String requestMessage) {
-        this.requestMessage = requestMessage;
-    }
-
-    public int getRequestId() {
-        return requestId;
-    }
-
-    public void setRequestId(int requestId) {
-        this.requestId = requestId;
-    }
-
-    public Date getRequestDate() {
-        return requestDate;
-    }
-
-    public void setRequestDate(Date requestDate) {
-        this.requestDate = requestDate;
-    }
-    
-    
+    }    
 }
